e9fdb80aa2779cb067253bdbed9ba484a7d690d3
galfan <galfan18kasec@gmail.com>
2021-01-22 15:17:17 -0600

    Adding new changes to this version

---
+++ assets/bienvenidos.2e438278.js
@@ -0,0 +1,1 @@
+import{o as n,c as i,e as o}from"./index.8df0d632.js";const e={};e.render=function(o,e){return n(),i("p",null,"Bienvenidos a mi sitio")},o(e).mount("#app");
---
+++ assets/getting-started.45a7a53b.js
@@ -0,0 +1,1 @@
+import{o as e,c as o,e as n}from"./index.8df0d632.js";const t={};t.render=function(n,t){return e(),o("h1",null,"Welcome to blog template")},n(t).mount("#app");
